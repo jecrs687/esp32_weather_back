e3f2055463642ef69d79a77ad3b251fbc48a406d
Emanuel Cascone <emanuelcascone@gmail.com>
2023-08-27 22:14:12 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import event from 'events';
+import cors from 'cors';
+const PORT = 3000;
+const app = express();
+
+var events = new event.EventEmitter();
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+    })
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    events.on('data', (data) => {
+        console.log(data);
+        res.send(data);
+    })
+
+    }
+);
+    
+app.post('/', (req, res) => {
+    events.emit('data', req.body)    
+    res.send('Data received');
+}
+);
