c868a2ccdeed72ea50863d9ecc2b7f6192df8e5a
Emanuel Cascone <emanuelcascone@gmail.com>
2023-08-29 08:30:26 -0300

    fix: data memory

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ const PORT = process.env.PORT || 3000 ;
 const app = express();
 
 var events = new event.EventEmitter();
-
+const minute = 60;
+const hour = 60 * minute;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
     })
 app.use(cors());
 app.use(express.json());
-var data = {};
+var data = [];
 app.get('/', (req, res) => {
     res.send(data);
     }
 );
 
 app.post('/', (req, res) => {
-    data = req.body;
+    Object.assign(req.body, {timestamp: Date.now()});
+    data.push(req.body);
+    if(data.length > hour*4){
+        data.shift();
+    }
     res.send("Data received");
 }
 );
